87f6c11eac3f6442805f68def769d0ba582b3f41
Thomas P <thomas.payen@i-carre.net>
2022-04-20 18:19:02 +0200

    Installation de l'orm

---
+++ src/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'e353379e3672a1256fcb1629c596a9dea1c0894e',
+    'name' => 'messagerie-melanie2/orm-api',
+  ),
+  'versions' => 
+  array (
+    'messagerie-melanie2/orm-api' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e353379e3672a1256fcb1629c596a9dea1c0894e',
+    ),
+    'messagerie-melanie2/orm-m2' => 
+    array (
+      'pretty_version' => '0.6.1.37',
+      'version' => '0.6.1.37',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '50f8cc50fde364bbd399cef5bc2131761d1e5882',
+    ),
+  ),
+);

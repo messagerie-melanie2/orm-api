57741e0da12cece72c027ece0a39e507f4cf9989
Thomas P <thomas.payen@i-carre.net>
2022-06-09 12:58:15 +0200

    Controller addressbook

--- src/controller/addressbook.php
+++ src/controller/addressbook.php
@@ -21,6 +21,162 @@
 
 namespace Controller;
 
+/**
+ * Classe de traitement pour les carnets d'adresses
+ * 
+ * @package Controller
+ */
 class Addressbook extends Controller {
-    
+    /**
+     * Récupération d'un carnet d'adresses
+     */
+    public static function get()
+    {
+        if (\Lib\Request::checkInputValues(['id'], \Lib\Request::INPUT_GET)) {
+            $user = \Lib\Utils::getCurrentUser();
+
+            $addressbook = \Lib\Objects::gi()->addressbook([$user]);
+            $addressbook->id = \Lib\Request::getInputValue('id', \Lib\Request::INPUT_GET);
+
+            if ($addressbook->load()) {
+                \Lib\Response::data(\Lib\Mapping::get('addressbook', $addressbook));
+            }
+            else {
+                \Lib\Response::error("Addressbook not found");
+            }
+        }
+    }
+
+    /**
+     * Récupération des contacts d'un carnet d'adresses
+     */
+    public static function contacts()
+    {
+        if (\Lib\Request::checkInputValues(['id'], \Lib\Request::INPUT_GET)) {
+            $user = \Lib\Utils::getCurrentUser();
+
+            $addressbook = \Lib\Objects::gi()->addressbook([$user]);
+            $addressbook->id = \Lib\Request::getInputValue('id', \Lib\Request::INPUT_GET);
+
+            if ($addressbook->load()) {
+                $contacts = $addressbook->getAllContacts();
+                $data = [];
+
+                foreach ($contacts as $contact) {
+                    $data[] = \Lib\Mapping::get('contact', $contact);
+                }
+
+                \Lib\Response::data($data);
+            }
+            else {
+                \Lib\Response::error("Addressbook not found");
+            }
+        }
+    }
+
+    /**
+     * Récupération des partages d'un carnet d'adresses
+     */
+    public static function shares()
+    {
+        if (\Lib\Request::checkInputValues(['id'], \Lib\Request::INPUT_GET)) {
+            $user = \Lib\Utils::getCurrentUser();
+
+            $addressbook = \Lib\Objects::gi()->addressbook([$user]);
+            $addressbook->id = \Lib\Request::getInputValue('id', \Lib\Request::INPUT_GET);
+
+            if ($addressbook->load()) {
+                $shares = \Lib\Objects::gi()->share([$addressbook]);
+                $is_group = \Lib\Request::getInputValue('is_group', \Lib\Request::INPUT_GET);
+
+                $shares->type = isset($is_group) && $is_group ? \LibMelanie\Api\Defaut\Share::TYPE_GROUP : \LibMelanie\Api\Defaut\Share::TYPE_USER;
+                $data = [];
+                foreach ($shares->getList() as $share) {
+                    $data[] = [
+                        'user' => $share->name,
+                        'acl' => $share->acl,
+                    ];
+                }
+                \Lib\Response::data($data);
+            }
+            else {
+                \Lib\Response::error("Addressbook not found");
+            }
+        }
+    }
+
+    /**
+     * Enregistrer/modifier un carnet d'adresses
+     */
+    public static function post()
+    {
+        $json = \Lib\Request::readJson();
+
+        if (isset($json) && $json !== false) {
+
+            if (\Lib\Request::checkInputValues(['id', 'name'], null, $json)) {
+                // Forcer l'uid dans le cas d'un user Basic
+                if (\Lib\Request::issetUser()) {
+                    $user = \Lib\Objects::gi()->user();
+                    $user->uid = \Lib\Request::getUser();
+                }
+                else {
+                    if (isset($json['owner'])) {
+                        $user = \Lib\Objects::gi()->user();
+                        $user->uid = $json['owner'];
+                    }
+                    else {
+                        \Lib\Response::error("Missing parameter owner");
+                        return;
+                    }
+                }
+
+                $addressbook = \Lib\Objects::gi()->addressbook([$user]);
+                $addressbook->id = $json['id'];
+
+                if (!$addressbook->load()) {
+                    $addressbook->owner = $user->uid;
+                }
+                
+                $addressbook->name = $json['name'];
+
+                $ret = $addressbook->save();
+
+                if (!is_null($ret)) {
+                    \Lib\Response::success(true);
+                }
+                else {
+                    \Lib\Response::error("Error when saving the addressbook");
+                }
+            }
+        }
+        else {
+            \Lib\Response::error("Invalid json parameter");
+        }
+    }
+
+    /**
+     * Suppression d'un carnet d'adresses
+     */
+    public static function delete()
+    {
+        if (\Lib\Request::checkInputValues(['id'], \Lib\Request::INPUT_GET)) {
+            $user = \Lib\Utils::getCurrentUser();
+
+            $addressbook = \Lib\Objects::gi()->addressbook([$user]);
+            $addressbook->id = \Lib\Request::getInputValue('id', \Lib\Request::INPUT_GET);
+
+            if ($addressbook->load()) {
+                if ($addressbook->delete()) {
+                    \Lib\Response::success(true);
+                }
+                else {
+                    \Lib\Response::error("Error when deleting addressbook");
+                }
+            }
+            else {
+                \Lib\Response::error("Addressbook not found");
+            }
+        }
+    }
 }
